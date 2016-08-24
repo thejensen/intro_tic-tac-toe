30907268ac8fbf97577d4b4de1133a5d50f44ceb
Satchel Grant and Sara Jensen <sara@>
2016-08-24 13:45:11 -0700

    center table and headers, add logic to display x and o on the table with clicks instead of inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,36 @@
 // Business Logic
 function Board(){
-	this.boardArray = [["","",""],
-										 ["","",""],
-										 ["","",""]];
-
-  this.lastCharacter = '';
-  this.goodMove = true;
+	this.boardArray = [["","",""],["","",""],["","",""]];
+  this.currentPlayer = 'x';
 }
 Board.prototype.setCharacter = function(char, row, col){
-  if (char !== this.lastCharacter) {
-	  this.boardArray[row][col] = char;
-    this.goodMove = true;
-    this.lastCharacter = char;
-  }
-  else {
-    this.goodMove = false;
-  }
+  this.boardArray[row][col] = char;
+	if (char == 'x') {
+		this.currentPlayer = 'o';
+	} else {
+		this.currentPlayer = 'x';
+	}
 }
 
 // UI Logic
 $(document).ready(function() {
-  $('form').submit(function(event) {
-    event.preventDefault();
-		var newBoard = new Board();
-
-		for (var rowNumber = 0; rowNumber < 3; rowNumber++){
-			for (var colNumber = 0; colNumber < 3; colNumber++){
-		  	var input = $('#r'+ rowNumber + '-c' + colNumber).val();
-		  	newBoard.setCharacter(input, rowNumber, colNumber);
-	  	}
-		}
-
-		alert(newBoard.boardArray);
+	var newBoard = new Board();
+  $('td.row0').click(function() {
+		var colInput = $(this).index();
+		$(this).text(newBoard.currentPlayer);
+		newBoard.setCharacter(newBoard.currentPlayer,0,colInput);
+		$("span#current-player").text(newBoard.currentPlayer.toUpperCase());
   });
+	$('td.row1').click(function() {
+		var colInput = $(this).index();
+		$(this).text(newBoard.currentPlayer);
+		newBoard.setCharacter(newBoard.currentPlayer,1,colInput);
+		$("span#current-player").text(newBoard.currentPlayer.toUpperCase());
+	});
+	$('td.row2').click(function() {
+		var colInput = $(this).index();
+		$(this).text(newBoard.currentPlayer);
+		newBoard.setCharacter(newBoard.currentPlayer,2,colInput);
+		$("span#current-player").text(newBoard.currentPlayer.toUpperCase());
+	});
 });
